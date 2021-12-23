b367a943fd88e1755c5a08a4d9c2dc292fcb17a9
OfficialAhmed <52674147+OfficialAhmed@users.noreply.github.com>
2021-12-23 09:51:45 +0400

    Add files via upload

---
+++ game icon formater/image formater.py
@@ -0,0 +1,14 @@
+import os
+
+path = os.getcwd() + "\\game icon formater"
+files = os.listdir(path)
+
+with open("json.txt", "w+") as text:
+    for file in files:
+        if ".png" in file or ".PNG" in file:
+            edit = file[: file.find(".png")].title()
+            if "-" in file:
+                edit = edit.replace("-", " ")
+            text.write('"' + edit + '",')
+
+            os.rename(path + "\\" + file, path + "\\edited\\" + edit + ".png")
---
+++ game icon js/customJS.js
@@ -0,0 +1,91 @@
+function fetchJSON() {
+  var xmlFile = new XMLHttpRequest();
+  xmlFile.open("get", "game icon data/icons.json", false);
+  xmlFile.send();
+  var xmlDoc = xmlFile.responseText;
+  const data = JSON.parse(xmlDoc);
+
+  return data;
+}
+
+function getImages(by = "") {
+  var imageType = document.getElementById("imageType").value;
+  var imagesFolder = "game icon images/" + imageType;
+
+  var iconSectionHeader = `<div class="u-expanded-width u-list u-list-2">
+                              <div class="u-repeater u-repeater-2">`;
+  var iconSectionFooter = `</div>
+                              </div>`;
+
+  var allImages = iconSectionHeader;
+  var iconGenerated = 0; // counter: every 12 icon generate footer and header for next 12 icons
+  var input = "";
+
+  if (by === "search") {
+    // by serach
+    input = document.getElementById("searchInput").value;
+    if (input === "") {
+      input = ".png";
+    }
+  } else {
+    //by alphabet
+    if (by === "") {
+      input = ".png";
+    } else {
+      input = document.getElementById("alphabet").value;
+    }
+  }
+
+  let data = fetchJSON();
+  var iconsList = data[imageType]["iconName"];
+  for (const index in iconsList) {
+    let icon = iconsList[index];
+
+    if (iconGenerated < 12) {
+      allImages +=
+        `<div class="u-align-center u-container-style u-effect-hover-zoom u-image-contain u-list-item u-repeater-item u-list-item-2">
+        <a download="` +
+        icon +
+        `.png" href="` +
+        imagesFolder +
+        "/" +
+        icon +
+        `.png" title="` +
+        icon +
+        `"
+            class="u-background-effect u-expanded">
+            <img loading="lazy" src='` +
+        imagesFolder +
+        "/" +
+        icon +
+        `.png' class="u-background-effect-image u-expanded u-image u-image-contain">
+            </a>
+            </div>
+          `;
+    } else {
+      allImages +=
+        iconSectionFooter +
+        iconSectionHeader +
+        `<div class="u-align-center u-container-style u-effect-hover-zoom u-image-contain u-list-item u-repeater-item u-list-item-2">
+        <a download="` +
+        icon +
+        `.png" href="` +
+        imagesFolder +
+        "/" +
+        icon +
+        `.png" title="@OfficialAhmed"
+            class="u-background-effect u-expanded">
+            <img loading="lazy" src='` +
+        imagesFolder +
+        "/" +
+        icon +
+        `.png' class="u-background-effect-image u-expanded u-image u-image-contain">
+            </a>
+            </div>
+          `;
+      iconGenerated = 0;
+    }
+    document.getElementById("iconSection").innerHTML = allImages;
+    iconGenerated += 1;
+  }
+}
