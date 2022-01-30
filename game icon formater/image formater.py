659f44ff645cdcaee6c64d101f70856860f7b657
OfficialAhmed <Ahmed.riyami1818@gmail.com>
2022-01-31 03:48:08 +0400

    Qz style added by anubis_qz

--- game icon formater/image formater.py
+++ game icon formater/image formater.py
@@ -1,14 +1,35 @@
-import os, json
+import os, json, time
+
+
+def timestamp(increment):
+    # unique number to avoid clashes in icons naming
+    result = ""
+    time.sleep(0.15)
+    x = time.localtime()
+    structure = (
+        x.tm_year,
+        x.tm_mon,
+        x.tm_mday,
+        x.tm_hour,
+        x.tm_min,
+        x.tm_sec,
+        str(round(time.time() * 1000) + increment)[-1:],
+    )
+
+    for i in structure:
+        result += f"{i}"
+
+    return result[2:]  # remove century
 
 
 def Run(iconType):
     path = os.getcwd()
     files = os.listdir(path)
-
     jsonPath = (
         path[: len(path) - len("game icon formater")] + "\\game icon data\\icons.json"
     )
 
+    counter = 1
     with open(path + "\\json.txt", "w+") as text:
         for file in files:
             if ".png" in file or ".PNG" in file:
@@ -24,33 +45,26 @@ def Run(iconType):
 
                 with open(jsonPath, "r+") as jsonFile:
                     data = json.load(jsonFile)
-                    targetArray = data[iconType]["iconName"]
-                    matchArray = [name for name in targetArray if edit in name]
-
-                    if len(matchArray) > 0:
-                        edit += " (" + str(len(matchArray) + 1) + ")"
+                    time.sleep(0.1)
+                    stamp = timestamp(counter)
+                    edit += f" ({stamp})"
 
                     try:
                         os.rename(
                             path + "\\" + file, path + "\\ready\\" + edit + ".png"
                         )
-                    except FileExistsError:
-                        numExist = int(edit[edit.find("(") + 1 : edit.find(")")])
-                        edit = edit[: edit.find("(")] + "(" + str(numExist + 1) + ")"
-                        os.rename(
-                            path + "\\" + file, path + "\\ready\\" + edit + ".png"
-                        )
-
-                    text.write('"' + edit + '",\n')
+                    except Exception as e:
+                        print(str(e))
+                    text.write(f"{edit}\n")
                     data[iconType]["iconName"].append(edit)
 
                     jsonFile.seek(0)
                     jsonFile.truncate()
                     json.dump(data, jsonFile)
+                    counter += 1
                 print("Added icon successfully.")
 
         os.startfile(path + "\\json.txt")
 
 
-Run(input("Enter the image type(3d/cover/ps5): ").lower())
-tmp = input("quit...")
+Run(input("Enter the image type(3d/cover/ps5/homebrew/system/qz): ").lower())
