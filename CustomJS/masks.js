7a5bf44adb26c38bf6390c680b856959b3f157e8
Officialahmed <Ahmed.riyami1818@gmail.com>
2023-07-17 23:11:00 +0400

    UPDATE: FACTORIZATION

--- CustomJS/masks.js
+++ CustomJS/masks.js
@@ -8,15 +8,19 @@ function render_masks(){
         
         if (!masks_section.includes('undefined')){
             masks_section += `
-            <div class="u-container-style u-hover-feature u-image u-image-default u-list-item u-repeater-item u-image-1" data-image-width="2000" data-image-height="1333">
-            <div class="u-container-layout u-similar-container u-container-layout-2 u-align-center" style="margin-top: 20%;">
-            <p class="u-text u-text-white u-align-center"> `+ name +` MASK DESIGNED BY</p>
-            <a href="https://twitter.com/`+ owner +`" class="u-align-center u-button-style u-text-hover-palette-4-light-1 u-text-palette-4-base"
-            >@`+ owner +`</a>
-            <img src="images/masks/mask/`+ name +`.png" alt="`+ name +` preview" height="362" width="362">
-            <form action="get" style="margin-left: 30%;"> 
-            <a href="images/masks/mask/`+ name +`.zip" class="u-border-none u-btn u-btn-round u-button-style u-hover-feature u-hover-palette-4-light-2 u-palette-4-base u-radius-4 u-text-hover-palette-4-base u-btn-3">Download</a>
-            </form>
+            <div class="u-repeater-item">
+                <div class="u-align-center" style="margin-top: 22%;">
+                    
+                    <p style="margin-top: -12%; margin-bottom: -1%;"> `+ name +`</p>
+                    
+                    <a href="https://twitter.com/`+ owner +`">@`+ owner +`</a>
+                    
+                    <img src="images/masks/mask/`+ name +`.png" height="256" width="256">
+                    
+                    <form action="get">
+                        <a class="neon_light_button" href="images/masks/mask/`+ name +`.zip">Download</a>
+                    </form>
+
                 </div>
             </div>
             `
